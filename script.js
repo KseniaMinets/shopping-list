ee9daf46a6f72697fc4df88e7926ca89c2eef834
KseniaMinets <minetsksenia@gmail.com>
2024-02-26 18:34:27 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -2,6 +2,33 @@
 
 /* Пустые элементы не должны добавляться */
 
+const sendInput = document.querySelector('#input'); 
+const itemsContainer = document.querySelector('.items');
+
+sendInput.addEventListener('keydown', function(event) { 
+    if (event.key == 'Enter') {
+        
+        const messageText = sendInput.value;
+
+        const newMessage = document.createElement('div');
+        newMessage.classList.add('item');
+        newMessage.textContent = messageText;
+        
+        if (messageText != '') {
+            itemsContainer.append(newMessage);
+        }
+    
+        sendInput.value = '';
+
+        const input = document.querySelectorAll('.item');
+        for (let item of input) {
+        item.classList.toggle('done'); 
+    }
+
+    }
+})
+
+
 /* Если кликнуть на элемент списка, он зачеркивается */
 
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
